2de7b8531c554a967a3eb00cdc3b5d3c156acd59
sanjai2208 <sanjaiboopathi2005@gmail.com>
2025-08-05 19:26:32 +0530

    Home page, dashboard completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 
+import AiTools from '../components/AiTools'
+import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
+import Testimonial from '../components/Testimonial'
+import Plan from "../components/Plan"
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <>
       <Navbar/>
+      <Hero/>
+      <AiTools/>
+      <Testimonial/>
+      <Plan/>
+      <Footer/>
     </>
   )
 }
